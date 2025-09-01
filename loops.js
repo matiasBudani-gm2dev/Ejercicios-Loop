c11d2bd039792f443425bad671c704e7263c3f87
Matias-Budani <matias.budani@gm2dev.com>
2025-09-01 11:37:50 -0300

    feat(loop.js): add exercise 5

--- loops.js
+++ loops.js
@@ -85,3 +85,14 @@ do{
     
 }while(!isNumber(numExercise4) || numExercise4 >= 0)
 saltoLinea()
+/* 5. Pedir un número hasta que se ingrese el número 0 o negativo, luego mostrar la suma de todos los números ingresados */
+let sumCounter = 0
+let numExercise5;
+do{
+    numExercise5 = prompt("Ingrese un numero positivo (ingrese 0 o numero negativo cuando haya terminado de ingresar numeros): ")
+    numExercise5.trim() !== '' ? numExercise5 = Number(numExercise5) : null
+    !isNumber(numExercise5) ? console.log("El sistema solo acepta numeros") :
+    numExercise5 > 0 ? sumCounter += numExercise5 : null
+    
+}while(!isNumber(numExercise5) || numExercise5 > 0)
+console.log(sumCounter + " es la suma de todos los numeros ingresados")
