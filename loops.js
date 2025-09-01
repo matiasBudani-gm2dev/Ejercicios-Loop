7bc7a3b5b76c513fe33f775d17269e333bb2725c
Matias-Budani <matias.budani@gm2dev.com>
2025-09-01 11:09:43 -0300

    feat(loops.js): add exercise 4

--- loops.js
+++ loops.js
@@ -69,3 +69,18 @@ for(let i = numExercise3; i > 0; i--){
     verifyPrimeNumber(i) ? console.log(i): null
 }
 saltoLinea()
+/* 4. Ingrese un número y mostrar su potencia n^2, repetir este proceso hasta que se
+ingrese un número negativo */
+let numExercise4;
+do{
+    numExercise4 = prompt("Ingrese un numero para calcular su potencia (ingrese uno negativo para terminar el proceso): ")
+    numExercise4 = Number(numExercise4)
+    if(!isNumber(numExercise4)){
+        console.log("El sistema solo acepta numeros") 
+    }else{
+        if(numExercise4 > 0){
+            console.log(Math.pow(numExercise4, 2))
+        }
+    }
+    
+}while(!isNumber(numExercise4) || numExercise4 > 0)
